92484fac0709f21375851b295802f45930f245db
muto <316663878@qq.com>
2019-04-28 12:45:40 +0800

    no message

---
+++ src/main/java/com/moebuff/jutils/lang/StringUtils.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright 2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.moebuff.jutils.lang;
+
+/**
+ * 字符串工具
+ *
+ * @author muto
+ * @since 1.0
+ */
+public class StringUtils {
+
+    public static final String EMPTY = "";
+
+    public static final String NULL_STRING = "" + null;
+
+    public static final String SPACE = " ";
+
+    public static final String LF = "\n"; // Linux
+
+    public static final String CR = "\r"; // MAC OS
+
+    public static final String CRLF = "\r\n"; // Windows
+
+    // 空值检查
+    //-----------------------------------------------------------------------
+
+    /**
+     * <pre>
+     * StringUtils.isEmpty(null)       = true
+     * StringUtils.isEmpty("")         = true
+     * StringUtils.isEmpty(" ")        = false
+     * StringUtils.isEmpty("alex")     = false
+     * StringUtils.isEmpty("  alex  ") = false
+     * </pre>
+     *
+     * @param cs
+     * @return
+     */
+    public static boolean isEmpty(final CharSequence cs) {
+        return cs == null || cs.length() == 0;
+    }
+
+    public static boolean isNotEmpty(final CharSequence cs) {
+        return !isEmpty(cs);
+    }
+
+    /**
+     * <pre>
+     * StringUtils.isEmpty(null)       = true
+     * StringUtils.isEmpty("")         = true
+     * StringUtils.isEmpty(" ")        = true
+     * StringUtils.isEmpty("alex")     = false
+     * StringUtils.isEmpty("  alex  ") = false
+     * </pre>
+     *
+     * @param cs
+     * @return
+     */
+    public static boolean isBlank(final CharSequence cs) {
+        int length;
+        if (cs == null || (length = cs.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < length; i++) {
+            if (!Character.isWhitespace(cs.charAt(i))
+            ) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isNotBlank(final CharSequence cs) {
+        return !isBlank(cs);
+    }
+
+    //
+    //-----------------------------------------------------------------------
+
+}
